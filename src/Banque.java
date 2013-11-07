1ecb9b20d3650ef77fbff6efc09e0f20dda31f8e
Landrydevy <landry.devy@gmail.com>
2013-11-07 22:57:42 +0100

    Update Banque.java
     to String

--- src/Banque.java
+++ src/Banque.java
@@ -43,7 +43,7 @@ public class Banque {
 	}
 	
 	// afficher les clients
-	 public void afficherClient() {
+	 public String toString() {
                 for (Client unClient : lesClients) {
                         System.out.println(Client.getNom() + " " + Client.getPrenom() + " est un client de cette banque.");
                 }
